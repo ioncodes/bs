b0b890677c155a4eb7b00ce9e5c288a16df3df15
Luca Marcelli <ionix@protonmail.com>
2018-01-09 17:56:16 +0100

    implement materializecss for register

---
+++ public/js/register.js
@@ -0,0 +1,13 @@
+var firstName = document.getElementById('firstname');
+var lastName = document.getElementById('lastname');
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+var verifyPassword = document.getElementById('verifypassword');
+
+function checkContent() {
+
+}
+
+function register() {
+
+}
