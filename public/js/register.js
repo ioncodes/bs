a59412e05ad834a1ad44e4df6d78981bb8228893
Luca Marcelli <ionix@protonmail.com>
2018-01-09 18:17:52 +0100

    implemented registration form logic

--- public/js/register.js
+++ public/js/register.js
@@ -4,10 +4,59 @@ var username = document.getElementById('username');
 var password = document.getElementById('password');
 var verifyPassword = document.getElementById('verifypassword');
 
-function checkContent() {
+var inputs = [
+  firstName,
+  lastName,
+  username,
+  password,
+  verifyPassword
+];
 
+function checkContent() {
+  for(let i = 0; i < inputs.length; i++) {
+    let input = inputs[i];
+    if(input.value === '') {
+      input.classList.remove('valid');
+      input.classList.add('invalid');
+    } else {
+      input.classList.remove('invalid');
+      input.classList.add('valid');
+    }
+  }
+}
+
+function checkPassword() {
+  if(password.value.length < 8) {
+    password.classList.remove('valid');
+    password.classList.add('invalid');
+  }
+  if(password.value !== verifyPassword.value || verifyPassword.value.length < 8) {
+    verifyPassword.classList.remove('valid');
+    verifyPassword.classList.add('invalid');
+  } else {
+    verifyPassword.classList.remove('invalid');
+    verifyPassword.classList.add('valid');
+  }
+}
+
+function validate() {
+  for(let i = 0; i < inputs.length; i++) {
+    let input = inputs[i];
+    if(input.classList.contains('invalid')) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function register() {
-
+  checkContent();
+  checkPassword();
+  let ret = validate();
+  if(ret) {
+    // send request
+      M.toast({html: 'Account created!'})
+  } else {
+    M.toast({html: 'Sorry, but there was an error!'})
+  }
 }
