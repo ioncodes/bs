391b147f13acd5ddb592e0572b1b841bf7f5b194
Luca Marcelli <ionix@protonmail.com>
2018-01-11 18:06:50 +0100

    cleanup

--- public/js/rooms.js
+++ public/js/rooms.js
@@ -1,8 +1,8 @@
 var roomsTable = document.getElementById('roomstable');
 
 window.onload = () => {
-  getRooms((rooms) => {
-    rooms.forEach((room) => {
+  getRooms(rooms => {
+    rooms.forEach(room => {
       let row = roomsTable.insertRow();
       let name = row.insertCell(0);
       let startValue = row.insertCell(1);
@@ -13,7 +13,20 @@ window.onload = () => {
       startValue.innerHTML = `$${room.start_value}`;
       date.innerHTML = 'todo';
       money.innerHTML = 'todo';
-      btn.outerHTML = `<a onclick="delete('${room.room_id}')" class="red waves-effect waves-light btn">delete</a>`
+      btn.outerHTML = `<a onclick="deleteRoom('${room.room_id}')" class="red waves-effect waves-light btn">delete</a>`
     });
+  });
+}
+
+function deleteRoom(roomId) {
+  post('/api/room/delete', {
+    room_id: roomId
+  }, (res) => {
+    if(res.status === 'ok') {
+      M.toast({html: 'Room deleted!'});
+      location.reload();
+    } else {
+      M.toast({html: 'Sorry, but there was an error!'})
+    }
   });
 }
